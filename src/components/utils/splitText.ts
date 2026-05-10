204029d117d9f42464cea282372df1e3430cffcb
THASMUDDIN <thasmu896@gmail.com>
2026-05-10 21:37:17 +0530

    Fixed SplitText TypeScript errors

--- src/components/utils/splitText.ts
+++ src/components/utils/splitText.ts
@@ -12,15 +12,23 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 
 export default function setSplitText() {
   ScrollTrigger.config({ ignoreMobileResize: true });
-  if (window.innerWidth < 900) return;
-  const paras: NodeListOf<ParaElement> = document.querySelectorAll(".para");
-  const titles: NodeListOf<ParaElement> = document.querySelectorAll(".title");
 
-  const TriggerStart = window.innerWidth <= 1024 ? "top 60%" : "20% 60%";
+  if (window.innerWidth < 900) return;
+
+  const paras: NodeListOf<ParaElement> =
+    document.querySelectorAll(".para");
+
+  const titles: NodeListOf<ParaElement> =
+    document.querySelectorAll(".title");
+
+  const TriggerStart =
+    window.innerWidth <= 1024 ? "top 60%" : "20% 60%";
+
   const ToggleAction = "play pause resume reverse";
 
   paras.forEach((para: ParaElement) => {
     para.classList.add("visible");
+
     if (para.anim) {
       para.anim.progress(1).kill();
       para.split?.revert();
@@ -32,7 +40,7 @@ export default function setSplitText() {
     });
 
     para.anim = gsap.fromTo(
-      para.split.words,
+      para.split?.words || [],
       { autoAlpha: 0, y: 80 },
       {
         autoAlpha: 1,
@@ -48,17 +56,20 @@ export default function setSplitText() {
       }
     );
   });
+
   titles.forEach((title: ParaElement) => {
     if (title.anim) {
       title.anim.progress(1).kill();
       title.split?.revert();
     }
+
     title.split = new SplitText(title, {
       type: "chars,lines",
       linesClass: "split-line",
     });
+
     title.anim = gsap.fromTo(
-      title.split.chars,
+      title.split?.chars || [],
       { autoAlpha: 0, y: 80, rotate: 10 },
       {
         autoAlpha: 1,
